1a2caa167a6115b6a17c4fdf0dd7689e91681923
Fernando Reis Guimarães <fernandobhz@gmail.com>
2021-07-28 15:32:47 -0300

    feature: port number && npx

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 require("http")
   .createServer((req, res) => res.writeHead(200).end("ok"))
-  .listen(process.env.PORT || 3000);
+  .listen(process.argv[2] || process.env.PORT || 3000);
