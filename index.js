5708c6c8b878366ba765acd09023c27ef9669913
Fernando Reis Guimarães <fernandobhz@gmail.com>
2023-02-02 16:27:32 -0300

    chore: print current iso date

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 require("http")
-  .createServer((req, res) => res.writeHead(200).end("ok"))
+  .createServer((req, res) => res.writeHead(200).end(new Date().toISOString()))
   .listen(process.argv[2] || process.env.PORT || 3000);
